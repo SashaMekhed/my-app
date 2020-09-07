aaab73eaddab8aed584c87944423ba5d6dfcfe1b
SashaMekhed <66962102+SashaMekhed@users.noreply.github.com>
2020-09-07 11:53:44 +0300

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Header.module.css';
+
+const Header = () => {
+    return <header className={classes.header}>
+        <img src="https://www.w3.org/html/logo/downloads/HTML5_Badge_256.png"></img>
+    </header>
+}
+
+export default Header;
